4f0a26d23943d6acad5229feeb6c6b7ad70811c2
danpatrickoneill <danpatrickoneill@gmail.com>
2020-07-17 21:31:34 -0400

    Added room model; fixed misnamed Character field

--- betrayal/models.py
+++ betrayal/models.py
@@ -9,10 +9,25 @@ class Character(models.Model):
   height = models.IntegerField()
   weight = models.IntegerField()
   birthday = models.DateField()
-  description = models.TextField(blank=True)
+  hobbies = models.CharField(max_length=100)
   speed = models.IntegerField()
   might = models.IntegerField()
   sanity = models.IntegerField()
   knowledge = models.IntegerField()
+
+class Room(models.Model):
+    ITEM = 'I'
+    MULTIPLE_ITEMS = 'II'
+    EVENT = 'E'
+    OMEN = 'O'
+    CARD_CHOICES = [
+      (ITEM, 'Item'),
+      (MULTIPLE_ITEMS, 'Multiple Items'),
+      (EVENT, 'Event'),
+      (OMEN, 'Omen'),
+    ]
+  name = models.CharField(max_length=100)
+  card = models.IntegerField(choices=CARD_CHOICES)
+  effect = models.CharField(max_length=100)
 
 

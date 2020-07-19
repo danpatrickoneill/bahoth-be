3acbb7fc6d603769435ed603a39bdf079ea967ea
danpatrickoneill <danpatrickoneill@gmail.com>
2020-07-18 20:48:02 -0400

    Added user field to Character model

--- betrayal/models.py
+++ betrayal/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
 class Character(models.Model):
+  user = user.OneToOneField(User, on_delete=models.CASCADE)
   name = models.CharField(max_length=100)
   description = models.TextField(blank=True)
   age = models.IntegerField()
